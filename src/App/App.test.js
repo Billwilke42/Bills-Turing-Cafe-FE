b4c4b1fe60bd32f6f044cd732e8f1ab3b5c2232b
Bill Wilke <billwilke2@gmail.com>
2020-07-16 11:59:31 -0600

    Updates dependencies and adds tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { render, waitFor, fireEvent } from '@testing-library/react';
-import { getReservations } from '../apiCalls';
+import { render, waitFor, fireEvent, getByText } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import { getReservations } from '../apiCalls';
 // jest.mock('../apiCalls')
 
 it('renders without crashing', () => {
@@ -12,8 +13,37 @@ it('renders without crashing', () => {
 });
 
 it('should render a form', async () =>  {
-  const { getByRole, getByPlaceholderText } = render(<App></App>)
+  const { getByText, getByPlaceholderText } = render(<App></App>)
 
-  const nameInput = await waitFor(() => getByPlaceholderText('Name')
- 
+  const nameInput = await waitFor(() => getByPlaceholderText('Name'))
+  const dateInput = await waitFor(() => getByPlaceholderText('Month/Day'))
+  const timeInput = await waitFor(() => getByPlaceholderText('Time'))
+
+  const reservationCard = await waitFor(() => getByText('Number of Guests: 3'))
+
+  expect(nameInput).toBeInTheDocument()
+  expect(dateInput).toBeInTheDocument()
+  expect(timeInput).toBeInTheDocument()
+
+  expect(reservationCard).toBeInTheDocument
+})
+
+it('should change when things are added to the inputs', async () =>  {
+  const { getByPlaceholderText, getByDisplayValue } = render(<App></App>)
+
+  const nameInput = await waitFor(() => getByPlaceholderText('Name'))
+  const dateInput = await waitFor(() => getByPlaceholderText('Month/Day'))
+  const timeInput = await waitFor(() => getByPlaceholderText('Time'))
+
+  fireEvent.change(nameInput, {target: {value: 'Bill'}})
+  fireEvent.change(dateInput, {target: {value: '10/19'}} )
+  fireEvent.change(timeInput, {target: {value: '7:00'}} )
+
+  const name = getByDisplayValue('Bill')
+  const date = getByDisplayValue('10/19')
+  const time = getByDisplayValue('7:00')
+
+  expect(name).toBeInTheDocument()
+  expect(date).toBeInTheDocument()
+  expect(time).toBeInTheDocument()
 })
